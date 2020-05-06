20e5bf777379a693e4b2f939e23db5175322363b
tagg-alex <62948085+tagg-alex@users.noreply.github.com>
2020-05-06 14:41:26 +0100

    Add files via upload

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/services/weather.dart';
+import 'city_screen.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+//FindLocation locationFinder = FindLocation();
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    WeatherModel weatherModel = WeatherModel();
+    var weatherData = await weatherModel.getLocationWeather();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitRipple(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
